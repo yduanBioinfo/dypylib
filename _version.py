41260b4f53c5d038f3caa74962d9c717ca4df827
yduanBioinfo <duanyou@outlook.com>
2020-09-18 16:27:50 +0800

    Fix a bug in _version.

--- _version.py
+++ _version.py
@@ -1,4 +1,5 @@
 #__version__ = "0.0.3"
 # Update version from VERSION file into module
-with open('VERSION', 'r') as fversion:
+import os
+with open(os.path.dirname(__file__)+'/VERSION', 'r') as fversion:
     __version__ = fversion.readline().rstrip()
