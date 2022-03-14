91b2453d445a672d772cb040c92a755d16b76560
yduanBioinfo <duanyou@outlook.com>
2022-03-14 21:56:44 +0800

    Add test for scripts/filter

---
+++ tests/test_scripts_filter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import filecmp
+from tempfile import NamedTemporaryFile
+from scripts.filter import main as filter_main
+
+def test_filter():
+    tmpfile = NamedTemporaryFile()
+    res_file = "tests/data/scripts_filter/res_file1_file2_filter.txt"
+    order = ["filter.py","-n1","-n2","-m","filter","tests/data/scripts_filter/file1.txt","tests/data/scripts_filter/file2.txt","-o",tmpfile.name]
+    filter_main(order)
+    assert filecmp.cmp(tmpfile.name, res_file)
+
+def test_diff():
+    tmpfile = NamedTemporaryFile()
+    res_file = "tests/data/scripts_filter/res_file1_file2_diff.txt"
+    order = ["filter.py","-n1","-n2","-m","differ","tests/data/scripts_filter/file1.txt","tests/data/scripts_filter/file2.txt","-o",tmpfile.name]
+    filter_main(order)
+    assert filecmp.cmp(tmpfile.name, res_file)
+
+def test_null():
+    tmpfile = NamedTemporaryFile()
+    res_file = "tests/data/scripts_filter/res_file1_file2_filter.txt"
+    order = ["filter.py","-n1","-n2","tests/data/scripts_filter/file1.txt","tests/data/scripts_filter/file2.txt","-o",tmpfile.name]
+    filter_main(order)
+    assert filecmp.cmp(tmpfile.name, res_file)
