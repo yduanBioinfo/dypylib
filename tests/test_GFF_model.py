5ccbb67b3b51e4836c5266c9fe2ed9f6a3825100
yduanBioinfo <duanyou@outlook.com>
2022-10-18 10:34:08 +0800

    Rearrange test functions

--- tests/test_GFF_model.py
+++ tests/test_GFF_model.py
@@ -6,11 +6,15 @@ from bio.seq.Annotation import create_genome_using_gffutils
 from bio.seq.Annotation import Chr, Gene, Transcript
 
 test_gtf = "tests/data/test.gtf"
-mygenome = create_genome_using_gffutils(test_gtf)
-mychr = mygenome['CI01000023']
-mygene = mychr['CIWT.8140']
+global mygenome, mychr, mygene
 
 def test_load_GFF():
+    global mygenome, mychr, mygene
+    mygenome = create_genome_using_gffutils(test_gtf)
+    mychr = mygenome['CI01000023']
+    mygene = mychr['CIWT.8140']
+
+def test_on_genome_object():
     target_keys = ['CI01000023', 'CI01000025']
     assert len(mygenome.keys()) == 2
     assert len(mygenome) == 2
@@ -21,7 +25,7 @@ def test_load_GFF():
     for k,v in mygenome.items():
         assert k in target_keys
 
-def test_Genome_getitem():
+def test_on_chr_object():
     assert isinstance(mychr, Chr)
     assert mychr.name == 'CI01000023'
     mychildren = mychr.get_children_id()
@@ -29,7 +33,28 @@ def test_Genome_getitem():
     assert 'CIWT.8168' in mychildren
     assert 'CIWT.8138' in mychildren
 
-def test_Genome_search():
+def test_on_Gene_object():
+    assert isinstance(mygene, Gene)
+    for gene in mychr.values():
+        for tx in gene.values():
+            assert isinstance(tx, Transcript)
+            assert isinstance(tx.name, str)
+
+def test_on_Tx_object():
+    assert 'CIWT.8140.asdfui' not in mygenome
+    assert 'CIWT.8140.asdfui' not in mygene
+    assert 'CIWT.8140' not in mygene
+    assert mygene.id == 'CIWT.8140'
+
+    # Test wrong key
+    with pytest.raises(KeyError):
+        mygenome['CIWT.8140.uioui']
+
+    for tx in mygene.values():
+        for i in tx.values():
+            assert i.featuretype in ['exon','cds']
+
+def test_search_method_of_Genome():
     assert isinstance(mygenome.search('CI01000023'),Chr)
     assert isinstance(mygenome.search('CIWT.8168'),Gene)
     assert isinstance(mygenome.search('CIWT.8138'),Gene)
@@ -39,23 +64,3 @@ def test_Genome_search():
     wrong_id = 'CIWT.814094'
     with pytest.raises(FeatureNotFoundError, match=wrong_id):
         mygenome.search(wrong_id)
-
-def test_Gene():
-    assert isinstance(mygene, Gene)
-    for gene in mychr.values():
-        for tx in gene.values():
-            assert isinstance(tx, Transcript)
-            assert isinstance(tx.name, str)
-
-def test_Tx():
-    assert 'CIWT.8140.asdfui' not in mygenome
-    assert 'CIWT.8140.asdfui' not in mygene
-    assert 'CIWT.8140' not in mygene
-    assert mygene.id == 'CIWT.8140'
-
-    with pytest.raises(KeyError):
-        mygenome['CIWT.8140.uioui']
-
-    for tx in mygene.values():
-        for i in tx.values():
-            assert i.featuretype in ['exon','cds']
